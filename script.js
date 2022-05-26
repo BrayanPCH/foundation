2b67ea2dfe0a2f6683b3991b38a3c0ffb78cb031
BrayanPCH <bapulamarinc@utn.edu.ec>
2022-05-26 16:19:10 -0500

    Modification to the design of the main page headers and connection to the api-rest to obtain the sizes/courses.

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ window.addEventListener("scroll", () => {
     TODO: JS CAROUSEL
     TODO: --------------------------------------------------------------------------------------------------------
 */
+
 // Initialize Swiper
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
@@ -48,7 +49,7 @@ if (window.innerWidth > 1100) {
             clickable: true,
         },
     });
-}else{
+} else {
     var swiper2 = new Swiper(".mySwiperS", {
         slidesPerView: 1,
         spaceBetween: 30,
@@ -59,3 +60,41 @@ if (window.innerWidth > 1100) {
         },
     });
 }
+
+/*
+    TODO: --------------------------------------------------------------------------------------------------------
+    TODO: JS OBTAIN COURSES
+    TODO: --------------------------------------------------------------------------------------------------------
+*/
+
+const URL = 'http://localhost:3000/talleres';
+
+const xhr = new XMLHttpRequest();
+
+function onRequestHandler() {
+    if (this.readyState == 4 && this.status == 200) {
+        const data = JSON.parse(this.response);
+        console.log(data);
+        const information = document.getElementById('courses-content');
+        let template = '';
+        data.forEach(publication => {
+            template += `<div class="courses-content__card">
+                            <div class="courses-content__card__course">
+                                <h3>${publication.titulo_tall}</h3>
+                                <div id="${publication.id_tall}"><img src="./img/img3.jpg" alt=""></div>
+                                <i class="fa fa-eye"></i>
+                                <p style="display: inline-block;">${publication.visitas_tall}</p>
+                                <button name="${publication.id_tall}" class="publication" onclick="openModal()">Ver Detalles
+                                </button>
+                                <button name="${publication.id_tall}" class="publication" onclick="openModal()">Inscribirme 
+                                </button>
+                            </div>
+                         </div>`;
+        });
+        information.innerHTML = template;
+    }
+}
+
+xhr.addEventListener("load", onRequestHandler);
+xhr.open("GET", `${URL}`);
+xhr.send();
