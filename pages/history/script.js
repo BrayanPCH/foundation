e5624f29a4a7098383c9ef0fbae25942a638570a
BrayanPCH <bapulamarinc@utn.edu.ec>
2022-07-25 15:49:34 -0500

    Home page design update.

--- pages/history/script.js
+++ pages/history/script.js
@@ -19,16 +19,38 @@
 //     });
 // });
 
-const btnCloseModal = document.querySelector('.close');
-const btnOpenModal = document.querySelector('.gallery ul li a');
+// const btnCloseModal = document.querySelector('.close');
+// const btnOpenModal = document.querySelector('.gallery ul li a');
 
-console.log(btnOpenModal);
+// console.log(btnOpenModal);
 
-btnCloseModal.addEventListener('click', c => {
-    console.log("Hola BB");
+// btnCloseModal.addEventListener('click', c => {
+//     console.log("Hola BB");
+// });
+
+// btnOpenModal.addEventListener('click', c => {
+//     console.log("Open");
+// });
+
+$(document).ready(function() {
+    console.log('ready');
+    // portfolio
+$('.gallery ul li a').click(function () {
+    var itemID = $(this).attr('href');
+    $('.gallery ul').addClass('item_open');
+    $(itemID).addClass('item_open');
+    return false;
+});
+$('.close').click(function () {
+    $('.port, .gallery ul').removeClass('item_open');
+    return false;
 });
 
-btnOpenModal.addEventListener('click', c => {
-    console.log("Open");
+$(".gallery ul li a").click(function () {
+    $('html, body').animate({
+        scrollTop: parseInt($("#top").offset().top)
+    }, 400);
 });
+});
+
 
--- script.js
+++ script.js
@@ -24,10 +24,10 @@ window.addEventListener("scroll", () => {
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
+    // autoplay: {
+    //     delay: 3000,
+    //     disableOnInteraction: false,
+    // },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
